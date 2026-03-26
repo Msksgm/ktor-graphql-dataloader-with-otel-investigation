51549ea10dcb8770e8b9a660a8b52d054cadf480
Msksgm <msksgm3124@gmail.com>
2026-03-27 08:57:09 +0900

    feat: implement routing configurations of graphql kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
     implementation(libs.ktor.server.netty)
     implementation(libs.logback.classic)
     implementation(libs.ktor.server.config.yaml)
+    implementation(libs.graphql.kotlin.ktor.server)
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
 }
--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,18 +1,39 @@
 package com.example
 
-import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
+import com.expediagroup.graphql.server.ktor.GraphQL
+import com.expediagroup.graphql.server.ktor.defaultGraphQLStatusPages
+import com.expediagroup.graphql.server.ktor.graphQLPostRoute
+import com.expediagroup.graphql.server.operations.Query
 import io.ktor.server.application.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.request.*
+import io.ktor.server.plugins.statuspages.StatusPages
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import org.jetbrains.exposed.sql.*
 
 fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello World!")
         }
+    }
+}
+
+class HelloWorldQuery: Query {
+    fun hello(): String = "Hello World!"
+}
+
+fun Application.graphQLModule() {
+    install(GraphQL) {
+        schema {
+            packages = listOf("com.example")
+            queries = listOf(
+                HelloWorldQuery()
+            )
+        }
+    }
+    routing {
+        graphQLPostRoute()
+    }
+    install(StatusPages) {
+        defaultGraphQLStatusPages()
     }
 }
